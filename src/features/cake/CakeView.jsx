e7df10d092ea4bc1d0e07917958a5836b8e65049
Amin Mirzapour <aminmrzpr@gmail.com>
2023-11-26 17:04:05 +0330

    first commit

---
+++ src/features/cake/CakeView.jsx
@@ -0,0 +1,10 @@
+const CakeView = () => {
+  return (
+    <div>
+        <h2>Number of Cakes : -</h2>
+        <button>Order Cake</button>
+        <button>Restock Cake</button>
+    </div>
+  )
+}
+export default CakeView
---
+++ src/features/user/UserView.jsx
@@ -0,0 +1,8 @@
+const UserView = () => {
+  return (
+    <div>
+        <h2>list of users : -</h2>
+    </div>
+  )
+}
+export default UserView
